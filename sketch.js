ff50075a9f580198e0ca4330f5aafb81dc59eeb2
PacmanTheDude <69361815+PacmanTheDude@users.noreply.github.com>
2020-09-14 17:19:57 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var dog, dog_img1, dog_img2;
+var food;
+
+function preload() {
+  dog_img1 = loadImage("images/dogImg.png");
+  dog_img2 = loadImage("images/dogImg1.png");
+
+}
+
+function setup() {
+  createCanvas(800, 700);
+  dog = createSprite(650,400,20,20);
+  dog.addImage(dog_img1);
+  dog.scale = 0.3;
+  food = createSprite(50,400,20,20);
+  food.shapeColor = "red";
+  
+}
+
+
+function draw() {  
+  drawSprites();
+}
+
+
+
